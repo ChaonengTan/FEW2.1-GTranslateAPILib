db4a95a4c31bc84e0dcacb3948d1cf93670aff6d
ChaonengTan <chaonengtan@gmail.com>
2021-05-03 23:30:01 -0700

    +translate(), +previous()

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 class gt{
     constructor(APIKEY){
-        this.APIKEY = APIKEY
+        this._APIKEY = APIKEY
         this.options = {
             method: 'POST',
             url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
             headers: {
                 'content-type': 'application/x-www-form-urlencoded',
                 'accept-encoding': 'application/gzip',
-                'x-rapidapi-key': `${this.APIKEY}`,
+                'x-rapidapi-key': `${this._APIKEY}`,
                 'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
                 useQueryString: true
             },
             form: undefined
         }
     }
-    translate(q, target, source){
-        this.options.form = {q: q, target: target, source: source}
-        const request = require('request')
-        request(this.options, function (error, response, body) {
-            if (error) throw new Error(error)
-            console.log(body)
-        })
-        return request
+    translate(query, target = 'en', source = null){
+        this.options.form = {q: query, target: target, source: source}
+        const request = require('request-promise-native')
+        return request(this.options)
+            .then(res => JSON.parse(res))
+            .then(res => {
+                console.log(res.data.translations[0].translatedText)
+                this.prevCall = res
+                return(res)
+            })
+            .catch(err => {
+                throw new Error(err)
+            })
     }
+    previous(){ if(this.prevCall) return this.prevCall }
 }
 module.exports = {
     gt:gt,
